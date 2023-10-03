84bcfe0770debf686b48f40501806eaf8260c23d
João Pedro De Meireles Alves <1349264@sga.pucminas.br>
2023-10-03 09:02:52 -0300

    atualização TP2

--- TP2/Jogador.java
+++ TP2/Jogador.java
@@ -1,5 +1,15 @@
-public class Morra {
+public class Jogador {
+
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+
     public static void main (String[] args) {
-        System.out.println("AAAAAAAAAAA");
+       
     }
 }
