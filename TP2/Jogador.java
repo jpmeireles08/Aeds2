7e38bd31cf224c7dbec895082624f197a6f8c74b
João Pedro De Meireles Alves <1349264@sga.pucminas.br>
2023-10-03 10:01:57 -0300

    att

--- TP2/Jogador.java
+++ TP2/Jogador.java
@@ -9,7 +9,99 @@ public class Jogador {
     private String cidadeNascimento;
     private String estadoNascimento;
 
-    public static void main (String[] args) {
-       
+    public Jogador() {
+        id = 0;
+        altura = 0;
+        peso = 0;
+        anoNascimento = 0;
+        nome = "";
+        universidade = "";
+        cidadeNascimento = "";
+        estadoNascimento = "";
+    }
+
+    public Jogador (int id, int altura, int peso, int anoNascimento, String nome, String universidade, 
+    String cidadeNascimento, String estadoNascimento) {
+        this.id = id;
+        this.altura = altura;
+        this.peso = peso;
+        this.anoNascimento = anoNascimento;
+        this.nome = nome;
+        this.universidade = universidade;
+        this.cidadeNascimento = cidadeNascimento;
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public void setAltura (int altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso (int peso) {
+        this.peso = peso;
+    }
+
+    public void setAnoNascimento (int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public void setUniversidade (String universidade) {
+        this.universidade = universidade;
+    }
+
+    public void setCidadeNascimento (String cidadeNascimento) {
+        this.cidadeNascimento = cidadeNascimento;
+    }
+
+    public void setEstadoNascimento (String estadoNascimento) {
+        this.estadoNascimento = estadoNascimento;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public int getAltura () {
+        return altura;
+    }
+
+    public int getPeso () {
+        return peso;
+    }
+
+    public int getAnoNascimento () {
+        return anoNascimento;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public String getUniversidade () {
+        return universidade;
+    }
+
+    public String getCidadeNascimento () {
+        return cidadeNascimento;
+    }
+
+    public String getEstadoNascimento () {
+        return estadoNascimento;
+    }
+
+    public void imprimir () {
+        System.out.println("[" + getId() + " ## " + getNome() + " ## " + getAltura() + " ## " + getPeso() 
+        + " ## " + getAnoNascimento() + " ## " + getUniversidade() + " ## " + getCidadeNascimento() + " ## " +
+        getEstadoNascimento() + "]");
+    }
+
+    public static void main(String[] args) {
+
     }
 }
